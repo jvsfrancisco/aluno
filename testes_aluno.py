17c0f6ecec2f747650e4b4b1a65abe1369eb4481
Benitex <benitoapepe@yahoo.com.br>
2024-06-19 15:49:47 -0300

    Adicionados unittests de aluno

--- testes_aluno.py
+++ testes_aluno.py
@@ -1,13 +1,47 @@
-from .aluno import *
+import unittest
+from aluno import *
 
-# TODO converter pra unit tests
+# Códigos de erro
+OPERACAO_REALIZADA_COM_SUCESSO = 0
 
-inicializar()
+ARQUIVO_NAO_ENCONTRADO = 30
+ARQUIVO_EM_FORMATO_INVALIDO = 31
+ERRO_NA_ESCRITA_DO_ARQUIVO = 32
 
-resultado, id = add_aluno('João', 'Centro', [8, 12])
-print(resultado, id)
-print(get_aluno(id))
-print(set_horario(id, 10, 12))
-print(get_aluno(id))
+ALUNO_NAO_ENCONTRADO = 16
 
-finalizar()
+class TesteFuncoesAluno(unittest.TestCase):
+    def teste_add_aluno(self):
+        resultado, id = add_aluno('Joao', 'Gavea', [8, 12])
+        self.assertEqual(resultado, 0)
+        self.assertEqual(
+            get_aluno(id),
+            (OPERACAO_REALIZADA_COM_SUCESSO, {'id': id, 'nome': 'Joao', 'filial_pref': 2, 'horario': [8, 12]})
+        )
+
+    def teste_get_aluno(self):
+        resultado, id = add_aluno('Maria', 'Gavea', [10, 14])
+        self.assertEqual(resultado, 0)
+        self.assertEqual(
+            get_aluno(id),
+            (OPERACAO_REALIZADA_COM_SUCESSO, {'id': id, 'nome': 'Maria', 'filial_pref': 2, 'horario': [10, 14]})
+        )
+
+    def teste_get_aluno_nao_encontrado(self):
+        resultado, aluno = get_aluno(-1)
+        self.assertEqual(resultado, ALUNO_NAO_ENCONTRADO)
+
+    def teste_set_horario(self):
+        resultado, id = add_aluno('Jose', 'Gavea', [8, 12])
+        set_horario(id, 10, 12)
+        self.assertEqual(
+            get_aluno(id),
+            (OPERACAO_REALIZADA_COM_SUCESSO,{'id': id, 'nome': 'Jose', 'filial_pref': 2, 'horario': [10, 12]})
+        )
+
+    def teste_set_horario_aluno_nao_encontrado(self):
+        resultado = set_horario(-1, 10, 12)
+        self.assertEqual(resultado, ALUNO_NAO_ENCONTRADO)
+
+if __name__ == '__main__':
+    unittest.main()
